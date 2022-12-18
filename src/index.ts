dcf0990b534ec10ece6f8e87d821ee92c0796bae
Jie Zhang <jiemartinez12@gmail.com>
2022-12-18 22:53:58 +0100

    no message

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import 'dotenv/config';
 import { ExtendedClient } from 'structures/Client';
 
+const keepAlive = require('../../server');
+// const Monitor = require('ping-monitor');
+ 
+keepAlive();
+// const monitor = new Monitor({
+// 	website: 'https://AlmibarHelper.xenox116.repl.co',
+// 	title: 'Nombre',
+// 	interval: 15 // minutes
+// });
+ 
+// monitor.on('up', (res: { website: any; }) => console.log(`${res.website} está encedido.`));
+// monitor.on('down', (res: { website: any; statusMessage: any; }) => console.log(`${res.website} se ha caído - ${res.statusMessage}`));
+// monitor.on('stop', (website: any) => console.log(`${website} se ha parado.`) );
+// monitor.on('error', (error: any) => console.log(error));
+
 new ExtendedClient().start();
